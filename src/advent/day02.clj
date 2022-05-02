8cd006cbb14b609684c261b315765104d86cf871
Eric Tobin <eatobin@gmail.com>
2022-05-02 14:00:28 -0500

    manipulate works

--- src/advent/day02.clj
+++ src/advent/day02.clj
@@ -2,10 +2,12 @@
   (:require [clojure.string :as str]))
 
 ; part a
+(defn manipulate [[dir amt]]
+  [(keyword (subs dir 0 1)) (Integer/parseInt amt)])
+
 (def v1 (->>
           "resources/day02.txt"
           (slurp)
           (str/split-lines)
           (map #(str/split % #" "))
-          (map #(Integer/parseInt (% 1)))
-          (into [])))
+          (map manipulate)))
